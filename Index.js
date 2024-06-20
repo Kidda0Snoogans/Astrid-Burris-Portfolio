9c16f80be2e1eadbd0a1779ebb0f0b661aeb743f
Kidda0Snoogans <mx.astridburris@gmail.com>
2024-06-20 09:56:27 -0400

    kill me plzz

---
+++ Index.js
@@ -0,0 +1,9 @@
+// To turn the intro French on hover
+
+let French_Intro = document.querySelector(".French_Intro")
+let Intro = document.querySelector(".Intro")
+
+Intro.addEventListener ("click", function() {
+    French_Intro.style.display = "block"
+    Intro.style.display = "none"
+})
