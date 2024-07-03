7d2810a82ed84bbd6ee8003361e1fcaaa44aa9a7
Kidda0Snoogans <mx.astridburris@gmail.com>
2024-07-03 01:36:22 -0400

    balls

--- Index.js
+++ Index.js
@@ -1,10 +1,16 @@
-// To turn the intro French on hover
+// To turn the Intro French on hover
 
-let French_Intro = document.querySelector(".French_Intro")
-let Intro = document.querySelector(".Intro")
+let Intro = document.querySelector(".Intro");
+let French_Intro = document.querySelector(".French_Intro");
 
-Intro.addEventListener ("click", function() {
-    French_Intro.style.display = "block"
-    Intro.style.display = "none"
-})
+Intro.addEventListener('mouseenter',function(){
+    Intro.style.display = "none";
+    French_Intro.style.display = "block";
+});
 
+// To turn back on exit
+
+French_Intro.addEventListener('mouseleave', function(){
+    Intro.style.display = "block";
+    French_Intro.style.display = "none";
+});
